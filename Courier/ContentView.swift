1b1c2c9681f2e8638fd4776b74f1374280819e13
Weixi779 <1349750521@qq.com>
2020-10-05 10:20:14 +0800

    Initial Commit

---
+++ Courier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Courier
+//
+//  Created by 孙世伟 on 2020/10/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Courier/CourierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourierApp.swift
+//  Courier
+//
+//  Created by 孙世伟 on 2020/10/5.
+//
+
+import SwiftUI
+
+@main
+struct CourierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
